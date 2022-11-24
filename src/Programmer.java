86b1847aa22d63df6e9527d42ad12f45aa131af8
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-11-24 02:30:51 -0500

    Added Programmer class attributes and constructor

--- src/Programmer.java
+++ src/Programmer.java
@@ -1,2 +1,12 @@
-public class Programmer {
+public class Programmer extends Employee {
+    final int GAIN_FACTOR_ERROR = 200;
+    int nbProjects;
+    int bonus;
+
+    public Programmer(String name, int birthYear, int nbProjects, int rate) {
+        super(name, birthYear, rate, "Programmer");
+        this.nbProjects = nbProjects;
+        System.out.println("We have a new employee: " + name + ", a " + getRole());
+
+    }
 }
