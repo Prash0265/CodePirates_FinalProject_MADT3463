d9a2206b0890d71f53ef1ab8a08bce4418d3a3aa
Prash0265 <kmrprashanth92@gmail.com>
2022-11-24 02:13:44 -0500

    Added getters and setters in Employee Class

--- src/Employee.java
+++ src/Employee.java
@@ -5,4 +5,36 @@ public class Employee {
     private String role;
     Vehicle employeeVehicle;
     //Contract employeeContract;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
