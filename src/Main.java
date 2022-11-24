13860ebaf336dc047ef041aa6234279c24ff9802
MeetWaraich <rwaraich599@gmail.com>
2022-11-24 02:19:40 -0500

    Added Manager attributes and employee array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+
+        Employee[] employees = new Employee[6];
+
+
+//        System.out.println("Test 1:");
+//        int length = employees.length;
+//        employees[0] = new Manager("Serge", 1985, 30, 4, 100);
+//        employees[1] = new Manager("Cindy", 1974, 20 , 6, 80) ;
+//        employees[2] = new Programmer("Paul", 1993, 3,  75);
+//        employees[3] = new Tester("Pierre", 1987, 124, 50 );
+//        employees[4] = new Programmer("Matt", 1981, 5 , 110);
+//        employees[5] = new Programmer("Sarah", 1986, 3 , 100);
     }
 }
--- src/Manager.java
+++ src/Manager.java
@@ -1,2 +1,7 @@
 public class Manager {
+    final int GAIN_FACTOR_CLIENT = 500;
+    final int GAIN_FACTOR_TRAVEL = 100;
+    int nbTravelDays;
+    int nbClients;
+    int bonus;
 }
