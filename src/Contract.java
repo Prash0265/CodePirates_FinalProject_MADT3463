c5e1ffe910c7aa12571791f0c11373cdac43474f
Prash0265 <kmrprashanth92@gmail.com>
2022-11-24 13:14:27 -0500

    Created a new class with name Permanent

--- src/Contract.java
+++ src/Contract.java
@@ -1,2 +1,10 @@
 public interface Contract {
 }
+class Permanent implements Contract {
+    int nbChildren;
+    boolean married;
+    int monthlySalary;
+    int bonusPerChildPerMonth;
+    int accumulatedDays;
+
+}
