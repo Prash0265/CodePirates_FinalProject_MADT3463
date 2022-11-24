c827ebc1b3a7f9cc386fb653f8fe490e6bccc1bb
Prash0265 <kmrprashanth92@gmail.com>
2022-11-24 02:10:02 -0500

    Added Employee Class attributes

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,8 @@
 public class Employee {
+    private String name;
+    private int birthYear;
+    private int rate;
+    private String role;
+    Vehicle employeeVehicle;
+    //Contract employeeContract;
 }
