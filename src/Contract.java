d0b4da275bee5b55cb6e8b135b5e406adf6183e3
MeetWaraich <rwaraich599@gmail.com>
2022-11-24 13:16:39 -0500

    Added getters and setters in Permanent class

--- src/Contract.java
+++ src/Contract.java
@@ -7,4 +7,44 @@ class Permanent implements Contract {
     int bonusPerChildPerMonth;
     int accumulatedDays;
 
+    public int getNbChildren() {
+        return nbChildren;
+    }
+
+    public void setNbChildren(int nbChildren) {
+        this.nbChildren = nbChildren;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public int getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(int monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public int getBonusPerChildPerMonth() {
+        return bonusPerChildPerMonth;
+    }
+
+    public void setBonusPerChildPerMonth(int bonusPerChildPerMonth) {
+        this.bonusPerChildPerMonth = bonusPerChildPerMonth;
+    }
+
+    public int getAccumulatedDays() {
+        return accumulatedDays;
+    }
+
+    public void setAccumulatedDays(int accumulatedDays) {
+        this.accumulatedDays = accumulatedDays;
+    }
+
 }
