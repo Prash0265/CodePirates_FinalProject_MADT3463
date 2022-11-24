33536979f381ac1b5cc53959d0ffbc8afc79e4be
AdityaRanjan466 <C0880466@mylambton.ca>
2022-11-24 02:40:52 -0500

    dded Motorcycle class attributes and constructor

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,2 +1,9 @@
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+    Boolean sidecar;
+
+    public Motorcycle(String make, String plate, String color, String category, Boolean sidecar) {
+        super(make, plate, color, category);
+        this.sidecar = sidecar;
+
+    }
 }
