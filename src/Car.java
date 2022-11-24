a36dd9804ec206c59707d7893ecf4ba0f26d4a3d
AdityaRanjan466 <C0880466@mylambton.ca>
2022-11-24 02:39:52 -0500

    dded Car class attributes and constructor

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,14 @@
-public class Car {
+public class Car extends Vehicle {
+    String gear;
+    String type;
+
+    public Car(String make, String plate, String color, String category, String gear, String type ) {
+
+        super(make, plate,color, category);
+        this.gear = gear;
+        this.type = type;
+
+    }
 
 
 }
