5d8043795c5bfc14901ed86a8aeae0b4f8e7d543
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-11-24 02:37:52 -0500

    dded Vehicle class attributes and constructor

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,12 @@
 public class Vehicle {
+    String make;
+    String plate;
+    String color;
+    String category;
+    public Vehicle(String make, String plate, String color, String category) {
+        this.make = make;
+        this.plate = plate;
+        this.color = color;
+        this.category = category;
+    }
 }
