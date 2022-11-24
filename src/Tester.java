b44a026910fb3d3e38368247902e0b7d5f653c09
MeetWaraich <rwaraich599@gmail.com>
2022-11-24 02:28:25 -0500

    Added Tester class attributes and constructor

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,12 @@
-public class Tester {
+public class Tester extends Employee {
+    final int GAIN_FACTOR_ERROR = 10;
+    int nbBugs;
+    int bonus;
+
+    public Tester(String name, int birthYear, int nbBugs, int rate) {
+        super(name, birthYear, rate, "Tester");
+        this.nbBugs = nbBugs;
+        System.out.println("We have a new employee: " + name + ", a " + getRole());
+
+    }
 }
